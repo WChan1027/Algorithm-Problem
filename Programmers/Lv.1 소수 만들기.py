a1653226d5669601da45e48b405dee231bddaa86
Suou <baewc95@gmail.com>
2022-08-07 23:46:09 +0900

    22.08.07 | Problem

---
+++ Programmers/Lv.1 소수 만들기.py
@@ -0,0 +1,29 @@
+'''
+https://school.programmers.co.kr/learn/courses/30/lessons/12977
+
+[문제]
+주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다. 
+숫자들이 들어있는 배열 nums가 매개변수로 주어질 때, nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
+
+
+[제한사항]
+nums에 들어있는 숫자의 개수는 3개 이상 50개 이하입니다.
+nums의 각 원소는 1 이상 1,000 이하의 자연수이며, 중복된 숫자가 들어있지 않습니다.
+
+'''
+
+def solution(nums):
+    answer = 0
+    for i in range(0, len(nums) - 2):
+        for j in range(i + 1, len(nums) - 1):
+            for k in range(j + 1, len(nums)):
+                num = nums[i] + nums[j] + nums[k]
+                small_number = 0
+                for l in range(2, num):
+                    if num % l == 0:
+                        small_number += 1
+                    if small_number != 0:
+                        break
+                if small_number == 0:
+                    answer += 1
+    return answer
