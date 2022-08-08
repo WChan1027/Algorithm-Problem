cb677112eaf671b5116359a40aa2072a030f5357
Suou <baewc95@gmail.com>
2022-08-08 23:45:57 +0900

    22.08.08 | Problem

--- Programmers/Lv.1 음양 더하기.py
+++ Programmers/Lv.1 음양 더하기.py
@@ -7,10 +7,10 @@ https://school.programmers.co.kr/learn/courses/30/lessons/76501
 
 
 [제한사항]
-absolutes의 길이는 1 이상 1,000 이하입니다.
-absolutes의 모든 수는 각각 1 이상 1,000 이하입니다.
-signs의 길이는 absolutes의 길이와 같습니다.
-signs[i] 가 참이면 absolutes[i] 의 실제 정수가 양수임을, 그렇지 않으면 음수임을 의미합니다.
+- absolutes의 길이는 1 이상 1,000 이하입니다.
+- absolutes의 모든 수는 각각 1 이상 1,000 이하입니다.
+- signs의 길이는 absolutes의 길이와 같습니다.
+- signs[i] 가 참이면 absolutes[i] 의 실제 정수가 양수임을, 그렇지 않으면 음수임을 의미합니다.
 
 '''
 
