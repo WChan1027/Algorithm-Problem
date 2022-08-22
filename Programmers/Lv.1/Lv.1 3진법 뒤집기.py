c3634b5b70e228e9ed23f5fa1a16a080e6dc95d2
Suou <baewc95@gmail.com>
2022-08-22 21:33:24 +0900

    22.08.22 | Problem

---
+++ Programmers/Lv.1/Lv.1 3진법 뒤집기.py
@@ -0,0 +1,26 @@
+'''
+https://school.programmers.co.kr/learn/courses/30/lessons/68935?language=python3
+
+[문제]
+
+자연수 n이 매개변수로 주어집니다.
+n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+
+
+[제한사항]
+
+- n은 1 이상 100,000,000 이하인 자연수입니다.
+
+'''
+
+def solution(n):
+    answer = 0
+    trit = []
+    while n > 0:
+        trit += [n % 3]
+        n = n // 3
+
+    for i in range(len(trit)):
+        answer += trit[i] * (3**(len(trit) - i - 1))
+
+    return answer
