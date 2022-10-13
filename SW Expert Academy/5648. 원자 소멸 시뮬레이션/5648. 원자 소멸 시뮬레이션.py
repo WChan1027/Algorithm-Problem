be1bb9da3cc6f597e9ebf9acb0604c90b9364c23
WChan <baewc95@gmail.com>
2022-10-13 17:55:43 +0900

    22.10.13 | Problem

---
+++ SW Expert Academy/5648. 원자 소멸 시뮬레이션/5648. 원자 소멸 시뮬레이션.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('sample_input.txt')
+
+T = int(input())
+
+for test_case in range(1, T):
+    N = int(input())
+
+    atom = [list(map(int, input().split())) for _ in range(N)]
+
+    answer = 0
+
