a154e7fc91a478637730e1e62bf13a5550ad6a99
Suou <baewc95@gmail.com>
2022-12-02 08:40:15 +0900

    22.12.02 | Problem

--- Baekjoon/Gold/14500. 테트로미노.py
+++ Baekjoon/Gold/14500. 테트로미노.py
@@ -4,5 +4,37 @@ sys.stdin = open('input.txt')
 
 N, M = map(int, sys.stdin.readline().split())
 
-paper = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
+paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
+direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+answer = 0
+visited = [[0] * M for _ in range(N)]
+
+def move(x, y, result, count):
+    global answer
+    if count == 4:
+        if result > answer:
+            answer = result
+        return
+
+    for i in range(4):
+        next_x = x + direction[i][0]
+        next_y = y + direction[i][1]
+        if 0 <= next_x < N and 0 <= next_y < M:
+            if visited[next_x][next_y] == 0:
+                if count == 2:
+                    visited[next_x][next_y] = 1
+                    move(x, y, result + paper[next_x][next_y], count + 1)
+                    visited[next_x][next_y] = 0
+                visited[next_x][next_y] = 1
+                move(next_x, next_y, result + paper[next_x][next_y], count + 1)
+                visited[next_x][next_y] = 0
+
+for i in range(N):
+    for j in range(M):
+        visited[i][j] = 1
+        move(i, j, paper[i][j], 1)
+        visited[i][j] = 0
+
+print(answer)
