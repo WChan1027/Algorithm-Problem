6d91c8441f23e6d3df94047c7e32e96ff05c286e
Suou <baewc95@gmail.com>
2023-01-09 08:12:18 +0900

    23.01.09

--- Baekjoon/Silver/6064. 카잉 달력.py
+++ Baekjoon/Silver/6064. 카잉 달력.py
@@ -8,52 +8,13 @@ T = int(input())
 for test_case in range(1, T+1):
     M, N, x, y = map(int, input().split())
 
-    mp = 1
-    find = 0
-    if max(M, N) == M:
-        candi = [x]
-        while True:
-            if (M * mp) % N == 0:
-                break
+    mp = 0
 
-            candi.append(M*mp + x)
-            mp += 1
-
-        for i in candi:
-            if i % N == y:
-                print(i)
-                find = 1
-    else:
-        candi = [y]
-        while True:
-            if (N * mp) % M == 0:
-                break
-
-            candi.append(N*mp + y)
-            mp += 1
-
-        for i in candi:
-            if i % M == x:
-                print(i)
-                find = 1
-
-    if find == 0:
-        print(-1)
-    # year = 1
-    # now_x, now_y = 1, 1
-    #
-    # while now_x != x or now_y != y:
-    #     if now_x == M and now_y == N:
-    #         break
-    #     now_x += 1
-    #     now_y += 1
-    #     if now_x > M:
-    #         now_x = 1
-    #     if now_y > N:
-    #         now_y = 1
-    #     year += 1
-    #
-    # if now_x == x and now_y == y:
-    #     print(year)
-    # else:
-    #     print(-1)
+    while True:
+        if (M * mp + x - y) % N == 0:
+            print(M * mp + x)
+            break
+        mp += 1
+        if (M * mp) % N == 0:
+            print(-1)
+            break
