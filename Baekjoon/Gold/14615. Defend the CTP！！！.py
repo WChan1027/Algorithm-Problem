0562d1a9b07b645b4f0f58bf91187678436e8ae1
WChan <baewc95@gmail.com>
2023-02-28 17:31:50 +0900

    23.02.28

--- Baekjoon/Gold/14615. Defend the CTP！！！.py
+++ Baekjoon/Gold/14615. Defend the CTP！！！.py
@@ -1,246 +1,63 @@
 # https://www.acmicpc.net/problem/14615
 import sys
-from collections import deque
 sys.stdin = open('input.txt')
+input = sys.stdin.readline
 
-N, M = map(int, sys.stdin.readline().split())
+N, M = map(int, input().split())
 
-tube = [[0] * (N+1) for _ in range(N+1)]
+tube = dict()
+tube_reverse = dict()
 
 for _ in range(M):
-    x, y = map(int, sys.stdin.readline().split())
-    tube[x][y] = 1
+    X, Y = map(int, input().split())
 
-# tube = [[] for _ in range(N+1)]
-#
-# for _ in range(M):
-#     x, y = map(int, sys.stdin.readline().split())
-#     tube[x].append(y)
+    if X in tube.keys():
+        tube[X] += [Y]
+    else:
+        tube[X] = [Y]
 
-T = int(sys.stdin.readline())
+    if Y in tube_reverse.keys():
+        tube_reverse[Y] += [X]
+    else:
+        tube_reverse[Y] = [X]
 
-for test_case in range(T):
-    bomb = int(sys.stdin.readline())
+stack = [1]
+visited = [0] * (N+1)
+visited[1] = 1
+find_bomb = [1]
 
-    queue = deque()
-    queue.append(1)
+while stack:
+    now = stack.pop()
 
-    visited = [0] * (N+1)
-    visited[1] = 1
-
-    complete = 0
-    while queue:
-        now = queue.popleft()
-        if tube[now][bomb] == 1:
-            complete = 1
-            break
-        for next in range(N+1):
-            if tube[now][next] == 1 and visited[next] == 0:
+    if now in tube.keys():
+        for next in tube[now]:
+            if visited[next] == 0:
                 visited[next] = 1
-                queue.append(next)
+                stack.append(next)
+                find_bomb.append(next)
+
+stack = [N]
+visited = [0] * (N+1)
+visited[N] = 1
+throw_bomb = [N]
+
+while stack:
+    now = stack.pop()
+
+    if now in tube_reverse.keys():
+        for next in tube_reverse[now]:
+            if visited[next] == 0:
+                visited[next] = 1
+                stack.append(next)
+                throw_bomb.append(next)
 
 
-    if complete == 1:
+T = int(input())
 
-        queue = deque()
-        queue.append(bomb)
+for num in range(T):
+    bomb = int(input())
 
-        visited = [0] * (N + 1)
-        visited[bomb] = 1
-
-        while queue:
-            now = queue.popleft()
-            if tube[now][N] == 1:
-                complete = 2
-                break
-            for next in range(N+1):
-                if tube[now][next] == 1 and visited[next] == 0:
-                    visited[next] = 1
-                    queue.append(next)
-
-    if complete == 2:
+    if bomb in find_bomb and bomb in throw_bomb:
         print('Defend the CTP')
     else:
         print('Destroyed the CTP')
-
-
-
-
-# for test_case in range(T):
-#     bomb = int(sys.stdin.readline())
-#
-#     que = deque()
-#     que.append(1)
-#
-#     visited = [0] * (N+1)
-#     visited[1] = 1
-#
-#     complete = 0
-#     end = 0
-#     while que:
-#         now = que.popleft()
-#         for next in tube[now]:
-#             if next == bomb:
-#                 complete = 1
-#                 end = 1
-#                 break
-#             if visited[next] == 0:
-#                 visited[next] = 1
-#                 que.append(next)
-#         if end == 1:
-#             break
-#
-#     if complete == 1:
-#         que = deque()
-#         que.append(bomb)
-#
-#         visited = [0] * (N+1)
-#         visited[bomb] = 1
-#
-#         while que:
-#             now = que.popleft()
-#             if N in tube[now]:
-#                 complete = 2
-#                 break
-#             for next in tube[now]:
-#                 if next == N:
-#                     complete = 2
-#                     end = 2
-#                     break
-#                 if visited[next] == 0:
-#                     visited[next] = 1
-#                     que.append(next)
-#             if end == 2:
-#                 break
-#
-#     if complete == 2:
-#         print('Defend the CTP')
-#     else:
-#         print('Destroyed the CTP')
-
-
-
-# for test_case in range(T):
-#     bomb = int(sys.stdin.readline())
-#
-#     que = deque()
-#     que.append(1)
-#
-#     visited = [0] * (N+1)
-#     visited[1] = 1
-#
-#     complete = 0
-#     while que:
-#         now = que.pop()
-#         if tube[now][bomb] == 1:
-#             complete = 1
-#             break
-#         for next in range(1, N+1):
-#             if tube[now][next] == 1 and visited[next] == 0:
-#                 visited[next] = 1
-#                 que.append(next)
-#
-#     if complete == 1:
-#         que = deque()
-#         que.append(bomb)
-#
-#         visited = [0] * (N+1)
-#         visited[bomb] = 1
-#
-#         while que:
-#             now = que.pop()
-#             if tube[now][N] == 1:
-#                 complete = 2
-#                 break
-#             for next in range(1, N+1):
-#                 if tube[now][next] == 1 and visited[next] == 0:
-#                     visited[next] = 1
-#                     que.append(next)
-#
-#     if complete == 2:
-#         print('Defend the CTP')
-#     else:
-#         print('Destroyed the CTP')
-
-
-# for test_case in range(T):
-#     bomb = int(sys.stdin.readline())
-#
-#     que = [0] * (N+1)
-#     que[1] = 1
-#
-#     visited = [0] * (N+1)
-#
-#     complete = 0
-#     end = 0
-#     while end == 0:
-#         if que[bomb] == 1:
-#             complete = 1
-#             break
-#         end = 1
-#         for now in range(1, N+1):
-#             if visited[now] == 0 and que[now] == 1:
-#                 end = 0
-#                 visited[now] = 1
-#                 for next in range(1, N+1):
-#                     if tube[now][next] == 1:
-#                         que[next] = 1
-#
-#
-#     if complete == 1:
-#         que = [0] * (N + 1)
-#         que[bomb] = 1
-#
-#         visited = [0] * (N+1)
-#
-#         end = 0
-#         while end == 0:
-#             if que[N] == 1:
-#                 complete = 2
-#                 break
-#             end = 1
-#             for now in range(1, N + 1):
-#                 if visited[now] == 0 and que[now] == 1:
-#                     end = 0
-#                     visited[now] = 1
-#                     for next in range(1, N + 1):
-#                         if tube[now][next] == 1:
-#                             que[next] = 1
-#
-#     if complete == 2:
-#         print('Defend the CTP')
-#     else:
-#         print('Destroyed the CTP')
-
-
-
-# def bfs(now, goal):
-#     que = deque()
-#     que.append(now)
-#
-#     visited = [0] * (N+1)
-#     visited[now] = 1
-#
-#     while que:
-#         a = que.pop()
-#         if tube[a][goal] == 1:
-#             return 1
-#
-#         for next in range(1, N+1):
-#             if tube[a][next] == 1 and visited[next] == 0:
-#                 visited[next] = 1
-#                 que.append(next)
-#
-#     return 0
-#
-# for test_case in range(T):
-#     bomb = int(sys.stdin.readline())
-#
-#     end = 0
-#     if bfs(1, bomb) == 1:
-#         if bfs(bomb, N) == 1:
-#             print('Defend the CTP')
-#             end = 1
-#
-#     if end == 0:
-#         print('Destroyed the CTP')
--- Baekjoon/Silver/27515. 1차원 2048과 쿼리.py
+++ Baekjoon/Silver/27515. 1차원 2048과 쿼리.py
@@ -5,8 +5,21 @@ input = sys.stdin.readline
 
 Q = int(input())
 
-count = dict()
 sum = 0
+
+def calc(a, b):
+    if 2*a > b and a <= b:
+        return a
+
+    result = a * a
+    if result < b:
+        return calc(result, b)
+    elif result == b:
+        return result
+    else:
+        c = calc(2, b // a)
+        return a * c
+
 
 for test_case in range(Q):
     query = input().strip()
@@ -14,77 +27,21 @@ for test_case in range(Q):
 
     if query[0] == '+':
         sum += num
-        a = sum
-        answer = 0
-        while a // 2 > 0:
-            answer += 1
-            a //= 2
 
-        print(2**answer)
+        if sum == 0:
+            print(0)
+        elif sum == 1:
+            print(1)
+        else:
+            print(calc(2, sum))
+
 
     else:
         sum -= num
 
-        a = sum
-        answer = 0
-        while a // 2 > 0:
-            answer += 1
-            a //= 2
-
         if sum == 0:
             print(0)
+        elif sum == 1:
+            print(1)
         else:
-            print(2**answer)
-
-    #
-    # count = dict(sorted(count.items()))
-    #
-    # if query[0] == '+':
-    #     if num in count.keys():
-    #         count[num] += 1
-    #         if num != 0:
-    #             if count[num] == 2:
-    #                 count[num] = 1
-    #                 while count[num] == 1:
-    #                     del count[num]
-    #                     num *= 2
-    #                     if num not in count.keys():
-    #                         count[num] = 0
-    #                 count[num] = 1
-    #
-    #     else:
-    #         count[num] = 1
-    #
-    #     keys = list(count.keys())
-    #     print(count)
-    #     # print(keys[-1])
-    #
-    # else:
-    #     if num in count.keys():
-    #         count[num] -= 1
-    #     else:
-    #         count[num] = -1
-    #
-    #     if count[num] < 0:
-    #         count[num] = 1
-    #         num *= 2
-    #         while num not in count.keys():
-    #             count[num] = 1
-    #             num *= 2
-    #
-    #         del count[num]
-    #     print(count)
-    #     keys = list(count.keys())
-    #
-    #     if count:
-    #         while count[keys[-1]] == 0:
-    #             del count[keys[-1]]
-    #             keys.pop()
-    #             if not count:
-    #                 break
-    #         if count:
-    #             print(keys[-1])
-    #         else:
-    #             print(0)
-    #     else:
-    #         print(0)
+            print(calc(2, sum))
