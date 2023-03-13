fd4354bd4567a570a12c210a3149708ae9c226dc
WChan <baewc95@gmail.com>
2023-03-13 17:58:06 +0900

    23.03.13

--- Baekjoon/Gold/17070. 파이프 옮기기 1.py
+++ Baekjoon/Gold/17070. 파이프 옮기기 1.py
@@ -10,32 +10,87 @@ visited = [[0] * N for _ in range(N)]
 visited[0][0] = 1
 visited[0][1] = 1
 
+for i in range(N):
+    for j in range(N):
+        if house[i][j] == 1:
+            visited[i][j] = 1
+
 answer = 0
 
 # 0: 우향, 1: 우하향, 2: 하향
-stack = [(0, 1, 0)]
 
-while stack:
-    now_x, now_y, dir = stack.pop()
+def pipe(x, y, d):
+    global answer, N
 
-    if dir == 0:
-        next_x = now_x
-        next_y = now_y + 1
-        next_dir = 0
+    if x == N-1 and y == N-1:
+        answer += 1
+        return
+
+    if d == 0:
+        next_x = x
+        next_y = y + 1
         if 0 <= next_x < N and 0 <= next_y < N and visited[next_x][next_y] == 0:
             visited[next_x][next_y] = 1
-            stack.append((next_x, next_y, next_dir))
+            pipe(next_x, next_y, 0)
+            visited[next_x][next_y] = 0
 
-        next_x = now_x + 1
-        next_y = now_y + 1
-        next_dir = 1
-        if 0 <= next_x < N and 0 <= next_y < N and visited[next_x][next_y] == 0 and visited[next_x - 1][next_y] == 0 and visited[next_x][next_y - 1]:
+        next_x = x + 1
+        next_y = y + 1
+        if 0 <= next_x < N and 0 <= next_y < N and visited[next_x][next_y] == 0 and visited[next_x - 1][next_y] == 0 and visited[next_x][next_y - 1] == 0:
             visited[next_x][next_y] = 1
             visited[next_x - 1][next_y] = 1
             visited[next_x][next_y - 1] = 1
+            pipe(next_x, next_y, 1)
+            visited[next_x][next_y] = 0
+            visited[next_x - 1][next_y] = 0
+            visited[next_x][next_y - 1] = 0
 
+    elif d == 1:
+        next_x = x
+        next_y = y + 1
+        if 0 <= next_x < N and 0 <= next_y < N and visited[next_x][next_y] == 0:
+            visited[next_x][next_y] = 1
+            pipe(next_x, next_y, 0)
+            visited[next_x][next_y] = 0
 
+        next_x = x + 1
+        next_y = y + 1
+        if 0 <= next_x < N and 0 <= next_y < N and visited[next_x][next_y] == 0 and visited[next_x - 1][next_y] == 0 and visited[next_x][next_y - 1] == 0:
+            visited[next_x][next_y] = 1
+            visited[next_x - 1][next_y] = 1
+            visited[next_x][next_y - 1] = 1
+            pipe(next_x, next_y, 1)
+            visited[next_x][next_y] = 0
+            visited[next_x - 1][next_y] = 0
+            visited[next_x][next_y - 1] = 0
 
-    elif dir == 1:
+        next_x = x + 1
+        next_y = y
+        if 0 <= next_x < N and 0 <= next_y < N and visited[next_x][next_y] == 0:
+            visited[next_x][next_y] = 1
+            pipe(next_x, next_y, 2)
+            visited[next_x][next_y] = 0
 
     else:
+        next_x = x + 1
+        next_y = y + 1
+        if 0 <= next_x < N and 0 <= next_y < N and visited[next_x][next_y] == 0 and visited[next_x - 1][next_y] == 0 and \
+                visited[next_x][next_y - 1] == 0:
+            visited[next_x][next_y] = 1
+            visited[next_x - 1][next_y] = 1
+            visited[next_x][next_y - 1] = 1
+            pipe(next_x, next_y, 1)
+            visited[next_x][next_y] = 0
+            visited[next_x - 1][next_y] = 0
+            visited[next_x][next_y - 1] = 0
+
+        next_x = x + 1
+        next_y = y
+        if 0 <= next_x < N and 0 <= next_y < N and visited[next_x][next_y] == 0:
+            visited[next_x][next_y] = 1
+            pipe(next_x, next_y, 2)
+            visited[next_x][next_y] = 0
+
+pipe(0, 1, 0)
+
+print(answer)
