720bc3242ea68049ca1b4c52cd820cc6573d041a
WChan <baewc95@gmail.com>
2023-03-17 18:00:17 +0900

    23.03.17

--- Baekjoon/Platinum/1510. 이등변 삼각형.py
+++ Baekjoon/Platinum/1510. 이등변 삼각형.py
@@ -1,14 +1,18 @@
 # https://www.acmicpc.net/problem/1510
 import sys
 from itertools import combinations
+import math
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
+
+
 N, M = map(int, input().split())
 
 answer = 0
 
 dots = list(i for i in range(N*M))
 
+tri = 0
 for triangle in combinations(dots, 3):
     a, b, c = triangle
     a_x, a_y = a // M, a % M
@@ -26,6 +30,9 @@ for triangle in combinations(dots, 3):
 
             if len(set([length1, length2, length3])) < 3:
                 answer += 1
+                if max(a_x, b_x, c_x) == N-1 and max(a_y, b_y, c_y) == M-1 and min(a_x, b_x, c_x) == 0 and min(a_y, b_y, c_y) == 0:
+                    print((a_x, a_y), (b_x, b_y), (c_x, c_y))
+                    tri += 1
 
         else:
             gradient1 = (c_y - a_y) / (c_x - a_x)
@@ -41,56 +48,63 @@ for triangle in combinations(dots, 3):
 
                 if len(set([length1, length2, length3])) < 3:
                     answer += 1
+                    if max(a_x, b_x, c_x) == N-1 and max(a_y, b_y, c_y) == M-1 and min(a_x, b_x, c_x) == 0 and min(a_y, b_y, c_y) == 0:
+                        print((a_x, a_y), (b_x, b_y), (c_x, c_y))
+                        tri += 1
+
+print(tri)
+print(answer)
+
+new = [[0] * (M+1) for _ in range(N+1)]
+
+for i in range(2, N+1):
+    for j in range(2, M+1):
+        if i == j:
+            new[i][j] += (i-1) * 4
+            new[i][j] += (i//2-1) * 4
+        else:
+            if abs(i - j) < min(i-1, j-1):
+                new[i][j] += 4
+
+            a, b = min(i-1, j-1), max(i-1, j-1)
+            theta = math.atan(a/b)
+            x = a / math.sin(2 * theta)
+            if not x % 1:
+                new[i][j] += 4
+
+            c = (b ** 2 - a ** 2) ** (1/2)
+            if not c % 1:
+                new[i][j] += 4
+
+            for x in range(1, a):
+                d = ((b ** 2 + x ** 2) - (a ** 2)) ** (1/2)
+                if not d % 1 and d < b:
+                    new[i][j] += 4
+
+                e = ((b ** 2 + x ** 2) - ((a - x) ** 2)) ** (1 / 2)
+                if not e % 1 and e < b:
+                    new[i][j] += 4
+
+                # for y in range(1, b):
+                #     if (a ** 2 + y ** 2) == ((a - x) ** 2) + ((b - y) ** 2):
+                #         new[i][j] += 4
+
+        if i % 2:
+            new[i][j] += 2
+
+        if j % 2:
+            new[i][j] += 2
+
+
+
+
+for a in new:
+    print(*a)
+
+
+answer = 0
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        answer += new[i][j] * (N + 1 - i) * (M + 1 - j)
 
 print(answer)
-#
-# n = [[0] * (M+1) for _ in range(N+1)]
-# n[2][2] = 4
-#
-# for i in range(3, N+1):
-#     n[i][2] = n[i-1][2] * 2 - n[i-2][2]
-#
-#     if i % 2 == 1:
-#         n[i][2] += 2
-#
-# for i in range(3, M+1):
-#     n[2][i] = n[2][i-1] * 2 - n[2][i-2]
-#
-#     if i % 2 == 1:
-#         n[2][i] += 2
-#
-# for i in range(3, N+1):
-#     for j in range(3, M+1):
-#         n[i][j] += n[i-1][j-1]
-#         n[i][j] += (n[i-1][j] - n[i-2][j]) * 2
-#         n[i][j] += (n[i][j-1] - n[i][j-2]) * 2
-#
-#
-#         if i % 2 == 1:
-#             n[i][j] += 2
-#
-#         if j % 2 == 1:
-#             n[i][j] += 2
-#
-#         if i == j:
-#             n[i][j] += (j-2) * 4
-#             n[i][j] += 4
-#         else:
-#             if i > 2 and j > 2:
-#                 if 2*j > i or 2*i > j:
-#                     n[i][j] += 4
-#
-# for a in n:
-#     print(*a)
-
-
-
-# num = [[0] * (M+1) for _ in range(N+1)]
-# num[2][2] = 4
-#
-# for i in range(3, M):
-#     for j in range(2, i+1):
-#         num[2][i] += n[2][j] * (i+1-j)
-#
-# for a in num:
-#     print(*a)
