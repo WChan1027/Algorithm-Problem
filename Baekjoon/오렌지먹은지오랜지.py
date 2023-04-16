d3e85aadaf6fb7dea424fbfae62a623ca8b75054
WChan <baewc95@gmail.com>
2023-04-16 14:37:58 +0900

    23.04.16

--- Baekjoon/오렌지먹은지오랜지.py
+++ Baekjoon/오렌지먹은지오랜지.py
@@ -5,25 +5,10 @@ input = sys.stdin.readline
 N = int(input())
 word = input()
 
-def orange(word):
-    for i in range(1, N):
-        if word[0] == word[i]:
-            check = 0
-            end = 0
-            for k in range(1, N - i):
-                if word[k] != word[i+k]:
-                    if check == 0:
-                        check = 1
-                    else:
-                        break
-                if k == N-i-1:
-                    end = 1
-            if check == 1 and end == 1:
-                return 1
+def orange(word, N):
 
-    return 0
 
-if orange(word) == 1:
+if orange(word, N) == 1:
     print('YES')
 else:
     print('NO')
