e56e8bd52cd5ea90e66c5454bada965bca426af9
WChan <baewc95@gmail.com>
2023-05-24 17:59:05 +0900

    23.05.24

--- Baekjoon/Gold/17281. ⚾.py
+++ Baekjoon/Gold/17281. ⚾.py
@@ -1,15 +1,56 @@
 import sys
 import itertools
-
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 N = int(input())
 
+# 9명의 선수 (0번 ~ 8번)
 players = list(range(9))
+# 각 선수의 이닝별 결과
 result = [list(map(int, input().split())) for _ in range(N)]
 
-players_comb = list(itertools.permutations(players, 9))
+# 0번 선수가 4번 타자가 되는 순서의 조합만 모은 리스트
+players_perm = list(p for p in itertools.permutations(players, 9) if p[3] == 0)
 answer = 0
 
-for order in players_comb:
+# 선수 순서에 따른 점수
+def play_baseball(game, player):
+    score = 0
+    idx = 0
+
+    # 각 이닝
+    for inning in game:
+        # 아웃카운트 초기화
+        out = 0
+        # 안타 치는 리스트
+        base = []
+
+        # 3 아웃이 될 때까지 진행
+        while out < 3:
+            if inning[player[idx]] == 0:
+                out += 1
+            else:
+                base.append(inning[player[idx]])
+
+            idx = (idx + 1) % 9
+
+        # 출루한 선수의 수만큼 점수 더하기
+        score += len(base)
+
+        # 홈까지 진루하지 못한 선수의 수만큼 점수 빼기
+        stay = 0
+        for i in base[::-1]:
+            stay += i
+            if stay < 4:
+                score -= 1
+            else:
+                break
+
+    return score
+
+
+for case in players_perm:
+    answer = max(play_baseball(result, case), answer)
+
+print(answer)
--- Baekjoon/Silver/1342. 행운의 문자열.py
+++ Baekjoon/Silver/1342. 행운의 문자열.py
@@ -1,25 +1,36 @@
 import sys
+from collections import defaultdict
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 S = input()
 answer = 0
 
-def check(word, last):
+def lucky(word, last_word):
     global answer
-    # if not word:
-    #     answer += 1
-    #     return
-
-    for str in word:
-        if str != last:
-            new_word = word.replace(str, "", 1)
-            if not new_word:
-                answer += 1
-                return
-            check(new_word, str)
-    else:
+    if not word:
+        answer += 1
         return
 
-check(S, 0)
+    for i in word:
+        if i != last_word:
+            word = word.replace(f'{i}', '', 1)
+            lucky(word, i)
+            word += f'{i}'
+    return
+
+EA = defaultdict(int)
+for i in S:
+    EA[i] += 1
+
+factorial = [1] * (len(S)//2)
+for i in range(1, len(S)//2 + 1):
+    factorial[i] = factorial[i-1] * i
+
+lucky(S, '0')
+
+for i in EA.values():
+    if i > 1:
+        answer //= factorial(i)
+
 print(answer)
